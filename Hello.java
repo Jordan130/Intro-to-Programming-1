60ef864c1b6e48aba427f2e5117b1cff61015c49
Jordan Rios <145077985+Jordan130@users.noreply.github.com>
2023-12-26 19:57:53 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+// Jordan Rios
+// Dr. Hardy
+// CSCI 161
+// Due: 1/25/22
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello, Jordan!");
+		System.out.println("The platapus doesn't have a stomach at all");
+		System.out.println("I am from York, PA");
+		System.out.println("I commute 45 minutes to school everyday");
+		System.out.println("I can play the trumpet");
+	}
+
+}
